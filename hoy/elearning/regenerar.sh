8089db1675eda6beba2abbf524c1206a277be126
Oscar GG <profesor.oscar.gomez@gmail.com>
2018-02-02 09:59:46 +0100

    Incluido HTML

---
+++ hoy/elearning/regenerar.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+cd apliweb
+cd examen-primera-mitad
+cd parte-ordenador
+make &
+
+cd ..
+cd parte-escrita
+make &
+
+
+cd ..
+cd ..
+cd ..
+
+cd marcas
+cd examen-primera-mitad
+cd parte-ordenador
+make & 
+
+cd ..
+cd parte-escrita
+make &
+
+
+cd ..
+cd ..
+cd ..
